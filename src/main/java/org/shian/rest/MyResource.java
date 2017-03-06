e5bb5c42fc0ea62efc36a8daf63298e878ace5d8
Shian Huang <shian.huang@manheim.com>
2017-03-06 07:31:39 -0800

    used path param and query param in the url

--- src/main/java/org/shian/rest/MyResource.java
+++ src/main/java/org/shian/rest/MyResource.java
@@ -1,22 +1,31 @@
 package org.shian.rest;
 
-import javax.inject.Singleton;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
-@Path("test")
-// this annotation tells the app to create only one instance
-@Singleton
+/*
+ * can't make this class to be Singleton, since Singleton instance
+ * will be created before the uri is constructed, so we can access
+ * the path param or query param
+ */
+@Path("{pathParam}/test")
 public class MyResource {
 	
-	private int count;
+	/*
+	 * The reason to put query param and path param in the class level
+	 * is that all the class methods can use them
+	 * 
+	 */
+	@PathParam("pathParam") private String pathParamExample;
+	@QueryParam("query") private String queryParamExample;
 	
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
 	public String testMethod() {
-		count++;
-		return "It works! Thsi method was called " + count + " time(s)";
+		return "It works! Path param used " + pathParamExample + " and Query param used " +  queryParamExample;
 	}
 }
